02fe43925b28549aeab2cbd25755dec91981abd0
Murali Krishna <mkrishna@legalzoom.com>
2019-04-07 12:39:37 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ModalModule,Modal,ModalContent,ModalFooter,ModalHeader } from 'ngx-modal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Modal';
+}
